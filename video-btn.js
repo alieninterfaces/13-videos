71d80b150d0e2986d8c72d60dfdcadd305874f92
cjgammon <cj.gammon@gmail.com>
2023-07-27 23:39:22 -0500

    finish

---
+++ video-btn.js
@@ -0,0 +1,66 @@
+class VideoHover extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+    this.addListeners();
+  }
+
+  render() {
+    this._shadowRoot.innerHTML = `
+                <style>
+                  .container {
+                  border-radius: 20px;
+                  overflow: hidden;
+                  cursor: pointer;
+                  transition: transform 0.4s ease-in-out;
+                  }
+
+                  .container:hover{
+                    transform: scale(1.1);
+                  }
+                    video {
+                        width: 100%;
+                        height: auto;
+                        display: block;
+                    }
+
+                    .bottom{
+                    font-size: 12px;
+                    background: linear-gradient(90deg, #0e192d, #09283c);
+                    padding: 15px 20px;
+                    box-sizing: border-box;
+                    }
+                </style>
+                <div class="container">
+                  <video muted>
+                      <source src="${this.getAttribute(
+                        "src"
+                      )}" type="video/mp4">
+                      Your browser does not support the video tag.
+                  </video>
+                  <div class="bottom">
+                    <slot></slot>
+                  </div>
+                </div>
+            `;
+  }
+
+  addListeners() {
+    const video = this._shadowRoot.querySelector("video");
+
+    this.addEventListener("mouseover", () => {
+      video.play();
+    });
+
+    this.addEventListener("mouseout", () => {
+      video.pause();
+      video.currentTime = 0;
+    });
+  }
+}
+
+customElements.define("video-btn", VideoHover);
